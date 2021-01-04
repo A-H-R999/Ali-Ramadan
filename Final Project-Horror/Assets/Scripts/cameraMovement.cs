95bbff84372af814d0f8edf1228cc551ecff9261
Ali Ramadan <aliramadan99kw@gmail.com>
2021-01-04 04:55:06 +0300

    Started working

---
+++ Final Project-Horror/Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    float rotationOnX;
+    float mouseSensitivity = 90f;
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Taking mouse input
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity;
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity;
+
+        //Rotate camera up & down
+        rotationOnX -= mouseY;
+        rotationOnX = Mathf.Clamp(rotationOnX, -90f, 90f);
+        transform.localEulerAngles = new Vector3(rotationOnX, 0f, 0f);
+
+        //Rotate camera left and right
+        player.Rotate(Vector3.up * mouseX);
+    }
+}
